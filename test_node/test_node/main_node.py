ec14a1ec6f3cf14938dbbe49c595d04a5ef26ce1
Ons2003 <onsouenniche6@gmail.com>
2023-11-28 16:09:30 +0100

    ee

---
+++ test_node/test_node/main_node.py
@@ -0,0 +1,72 @@
+import rclpy
+# import the ROS2 python libraries
+from rclpy.node import Node
+# import the Twist interface from the geometry_msgs package
+from geometry_msgs.msg import Twist
+import serial
+import time
+
+
+class SimplePublisher(Node):
+
+    def __init__(self):
+        # Here you have the class constructor
+        # call super() in the constructor to initialize the Node object
+        # the parameter you pass is the node name
+        super().__init__('simple_publisher')
+
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0)
+        ser.reset_input_buffer()
+        # create the publisher object
+        # in this case, the publisher will publish on /cmd_vel Topic with a queue size of 10 messages.
+        # use the Twist module for /cmd_vel Topic
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        # define the timer period for 0.5 seconds
+        timer_period = 1
+        # create a timer sending two parameters:
+        # - the duration between 2 callbacks (0.5 seconds)
+        # - the timer function (timer_callback)
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        
+
+
+    def timer_callback(self):
+        # Here you have the callback method
+        # create a Twist message
+        msg = Twist()
+        # define the linear x-axis velocity of /cmd_vel Topic parameter to 0.5
+        msg.linear.x = 0.5
+        # define the angular z-axis velocity of /cmd_vel Topic parameter to 0.5
+        msg.angular.z = 0.5
+        # Publish the message to the Topic
+        self.publisher_.publish(msg)
+
+        led_number = 1 
+        print("Sending number " + str(led_number) + " to Arduino.")
+
+        ser.write(str(led_number).encode('utf-8'))
+
+        line = ser.readline().decode('utf-8').rstrip()
+
+        print(line)
+
+        # Display the message on the console
+        self.get_logger().info('Publishing: "%s"' % line)
+    
+        
+            
+def main(args=None):
+    # initialize the ROS communication
+    rclpy.init(args=args)
+    # declare the node constructor
+    simple_publisher = SimplePublisher()
+    # pause the program execution, waits for a request to kill the node (ctrl+c)
+    rclpy.spin(simple_publisher)
+    # Explicity destroys the node
+    simple_publisher.destroy_node()
+    # shutdown the ROS communication
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
